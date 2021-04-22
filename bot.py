3945ba0d4542e4f840c9d2c93fc8844973865a3b
vitaly.skopets <$12PeReP0DViPoDVeRTaM56>
2021-04-22 17:54:28 +0300

    Add webhook in file bot.py

--- bot.py
+++ bot.py
@@ -33,9 +33,18 @@ def main():
     dp.add_handler(MessageHandler(Filters.regex('^(Пункты наблюдения)$'), scraper))
     dp.add_handler(MessageHandler(Filters.text, messages))
 
-    logger.info('Start Bot')
-    bot.start_polling()
-    bot.idle()
+    if os.environ['HEROKU_APP_NAME'] is None:
+        logger.info('Start Bot in pooling mode')
+        bot.start_polling()
+        bot.idle()
+    else:
+        bot.start_webhook(listen='0.0.0.0',
+                          port=8443,
+                          url_path=os.environ['TOKEN'],
+                          webhook_url=f"https://{os.environ['HEROKU_APP_NAME']}.herokuapp.com/{os.environ['TOKEN']}"
+                          )
+        logger.info('Start Bot in webhook mode')
+        bot.idle()
 
 
 if __name__ == '__main__':
