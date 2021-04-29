d53f259158429b106ad73ee736c4b10326944b48
vitaly.skopets <$12PeReP0DViPoDVeRTaM56>
2021-04-29 18:42:39 +0300

    Added docstrings in crypto() and decoder() functions from crypto.py file

--- crypto.py
+++ crypto.py
@@ -2,6 +2,11 @@ import rsa
 
 
 def crypto() -> None:
+    """
+    Функция генерирует пару ключей (публичный и приватный) и записывает их в файлы pubkey.pem и private.pem,
+    соответственно для дальнейшего ассиметричного RSA шифрования/дешифрования стркоовых объектов
+    :return: None
+    """
     (pubkey, privkey) = rsa.newkeys(512, accurate=False)
     pubkey_pem = pubkey.save_pkcs1()
     privkey_pem = privkey.save_pkcs1()
@@ -12,6 +17,15 @@ def crypto() -> None:
 
 
 def decoder(token: bytes) -> str:
+    """
+    Функия дешифрования строковых объектов - идентификацуионных данных пользователей, хранящихся в коллекции users
+    базы данных MongoDB Atlas в полях с ключами first_name, last_name и username. Данные дешифруются
+    с использованием приватного ключа, который читается из файла private.pem
+    :param token: байтовые строки - последовательность байт - зашифрованные идентификацуионные данные пользователей,
+    хранящиеся в коллекции users базы данных MongoDB Atlas в полях с ключами first_name, last_name и username
+    :return: строковый объект - дешифрованные идентификацуионные данные пользователей, хранящиеся в коллекции users
+    базы данных MongoDB Atlas в полях с ключами first_name, last_name и username
+    """
     with open('private.pem', mode='rb') as privatefile:
         keydata = privatefile.read()
     privkey = rsa.PrivateKey.load_pkcs1(keydata)
@@ -21,4 +35,5 @@ def decoder(token: bytes) -> str:
 
 if __name__ == '__main__':
     # crypto()
-    decoder()
+    decoder(b'')
+
